c083531ab0e9d4b6ee591b18b83cfed31f4ec7ae
Evgeniy Gordeev <89735790+egordeev-ds@users.noreply.github.com>
2023-05-14 23:37:57 +0300

    add image

--- myproject/forms.py
+++ myproject/forms.py
@@ -2,3 +2,4 @@ from django import forms
 
 class PostForm(forms.Form):
     text = forms.CharField(label='Введите текст', widget=forms.Textarea)
+    image = forms.ImageField(label='Введите изображение', required=False)
--- myproject/views.py
+++ myproject/views.py
@@ -1,18 +1,28 @@
+import sqlite3
+from PIL import Image
 from django.shortcuts import render, redirect
 from .forms import PostForm
 
 def post_view(request): #отправка запроса
     if request.method == 'POST':
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             text = form.cleaned_data['text']
-            # Сохраните текст в базу данных
-            # ...
+            image = form.cleaned_data['image']
+            
+            # Сохраните текст и изображение в базу данных
+            conn = sqlite3.connect('db.sqlite3')  # Подключаемся к базе данных
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO posts (text, image) VALUES (?, ?)", (text, image.read()))
+            conn.commit()  # Сохраняем изменения в базе данных
+            conn.close()  # Закрываем соединение с базой данных
+            
             # Перенаправьте пользователя на другую страницу
             return redirect('post_success')
     else:
         form = PostForm()
     return render(request, 'post.html', {'form': form})
+
 
 def post_success_view(request): #отправка запроса после валидации
     return render(request, 'post_success.html')
