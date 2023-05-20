a82007f6de13e5e74d9a3201f27f7f60fbd792cb
Evgeniy Gordeev <89735790+egordeev-ds@users.noreply.github.com>
2023-05-21 02:16:27 +0300

    NICE STYLE

--- static/search_bar.js
+++ static/search_bar.js
@@ -1,41 +1,57 @@
 // Функция для обработки нажатия клавиши Enter
 function handleKeyPress(event) {
-    if (event.keyCode === 13) { // Код клавиши Enter
-      handleSearch();
-    }
+  if (event.keyCode === 13) { // Код клавиши Enter
+    handleSearch();
   }
-  
-  // Функция для выполнения поиска
-  function handleSearch() {
-    const inputElement = document.getElementById('filter-input');
-    handleFilterChange(inputElement);
-    updateQuestionCount(); // Обновляем значение question.count
-  }
-  
-  // Функция для обработки изменений в фильтре
-  function handleFilterChange(inputElement) {
-    const filterValue = inputElement.value.trim().toLowerCase();
-    const questionTitles = document.querySelectorAll('.question-title');
-    questionTitles.forEach(function(title) {
-      const questionTitleText = title.textContent.toLowerCase();
-      if (questionTitleText.includes(filterValue)) {
-        title.parentNode.style.display = 'block';
-      } else {
-        title.parentNode.style.display = 'none';
-      }
-    });
-  }
-  
-  // Функция для обновления значения question.count
-  function updateQuestionCount() {
-    const questionCountElement = document.getElementById('question-count');
-    const visibleQuestions = document.querySelectorAll('.question-title');
-    const count = Array.from(visibleQuestions).filter(function(title) {
-      return title.parentNode.style.display !== 'none';
-    }).length;
-    questionCountElement.textContent = 'Total number of questions: ' + count;
-  }
-  
-  // Назначение обработчика события для поля ввода
+}
+
+// Функция для выполнения поиска
+function handleSearch() {
   const inputElement = document.getElementById('filter-input');
-  inputElement.addEventListener('keypress', handleKeyPress);  
+  const filterValue = inputElement.value.trim().toLowerCase();
+
+  // Отправляем запрос на сервер для поиска элементов в базе данных
+  fetch('/search', {
+    method: 'POST',
+    body: JSON.stringify({ filter: filterValue }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Обрабатываем полученные данные
+    handleFilterChange(data);
+    updateQuestionCount(); // Обновляем значение question.count
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
+// Функция для обработки изменений в фильтре
+function handleFilterChange(data) {
+  const questionTitles = document.querySelectorAll('.question-title');
+  questionTitles.forEach(function(title) {
+    const questionTitleText = title.textContent.toLowerCase();
+    if (data.includes(questionTitleText)) {
+      title.parentNode.style.display = 'block';
+    } else {
+      title.parentNode.style.display = 'none';
+    }
+  });
+}
+
+// Функция для обновления значения question.count
+function updateQuestionCount() {
+  const questionCountElement = document.getElementById('question-count');
+  const visibleQuestions = document.querySelectorAll('.question-title');
+  const count = Array.from(visibleQuestions).filter(function(title) {
+    return title.parentNode.style.display !== 'none';
+  }).length;
+  questionCountElement.textContent = 'Total number of questions: ' + count;
+}
+
+// Назначение обработчика события для поля ввода
+const inputElement = document.getElementById('filter-input');
+inputElement.addEventListener('keypress', handleKeyPress);
